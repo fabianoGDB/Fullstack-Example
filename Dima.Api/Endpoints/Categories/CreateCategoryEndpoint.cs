364e03e99bc1b3283e59609299a69f7121d4ac9b
fabianoGDB <doctorfabi.f@gmail.com>
2025-02-14 19:12:05 -0300

    make a pattern for endpointts

---
+++ Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dima.Api.Common.Api;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class CreateCategoryEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
