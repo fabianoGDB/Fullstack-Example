f484ccde5bf697b082280de74692c768b9e960b6
fabianoGDB <doctorfabi.f@gmail.com>
2025-02-25 23:33:19 -0300

    creating user mapping for identity api

--- Dima.Api/obj/Debug/net9.0/Dima.Api.AssemblyInfo.cs
+++ Dima.Api/obj/Debug/net9.0/Dima.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd20c3710039b6d73f2e750d8a978834253ee76b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90db79c415afc259f5424dc89d58d2e4444043a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Dima.Core/obj/Debug/net9.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net9.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd20c3710039b6d73f2e750d8a978834253ee76b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90db79c415afc259f5424dc89d58d2e4444043a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
