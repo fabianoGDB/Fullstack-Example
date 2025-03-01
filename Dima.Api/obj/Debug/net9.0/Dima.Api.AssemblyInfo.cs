b382697c208357d2cad08ff62027811b70ebfdc7
fabianoGDB <doctorfabi.f@gmail.com>
2025-02-28 23:47:44 -0300

    criating migration v2

--- Dima.Api/obj/Debug/net9.0/Dima.Api.AssemblyInfo.cs
+++ Dima.Api/obj/Debug/net9.0/Dima.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c497e05d38c68269599297d6bf6061d7c3c1f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72c0de0abc99be43dc4a4e7238aeb31b0eb6cbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
